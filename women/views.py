c46099976659fb447507716c19e6630c040bf11e
rundect <dofastsite@gmail.com>
2022-03-14 01:03:09 +0300

    added templates

--- women/views.py
+++ women/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 
 def index(request):  # HTTPRequest
-    return HttpResponse('Страница приложения women')
+    posts = Women.objects.all()
+    # return HttpResponse('Страница приложения women')
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):  # HTTPRequest
+    # return HttpResponse('Страница приложения women')
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
 def categories(request, catid):  # HTTPRequest
