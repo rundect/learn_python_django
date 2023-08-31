ddb6c54d6f9bfb0d750688e2da91698b89d2624b
rundect <dofastsite@gmail.com>
2023-09-01 00:18:05 +0300

    Add weather api

--- test_project/test_app/models.py
+++ test_project/test_app/models.py
@@ -4,3 +4,40 @@ from django.db import models
 class WeatherApi(models.Model):
     url = models.URLField()
     password = models.CharField(max_length=100)
+
+
+class WeatherLocation(models.Model):
+    name = models.CharField(max_length=50),
+    region = models.CharField(max_length=50),
+    country = models.CharField(max_length=50),
+    lat = models.DecimalField(max_digits=5, decimal_places=2),
+    lon = models.DecimalField(max_digits=5, decimal_places=2),
+    tz_id = models.CharField(max_length=50),
+    localtime_epoch = models.IntegerField(),
+    localtime = models.DateTimeField(),
+
+
+class WeatherInfo(models.Model):
+    last_updated_epoch = models.IntegerField(),
+    last_updated = models.DateTimeField(),
+    temp_c = models.DecimalField(max_digits=5, decimal_places=2),
+    temp_f = models.DecimalField(max_digits=5, decimal_places=2),
+    is_day = models.IntegerField(),
+    condition = models.JSONField(),
+    wind_mph = models.DecimalField(max_digits=5, decimal_places=2),
+    wind_kph = models.DecimalField(max_digits=5, decimal_places=2),
+    wind_degree = models.IntegerField(),
+    wind_dir = models.CharField(max_length=10),
+    pressure_mb = models.DecimalField(max_digits=5, decimal_places=2),
+    pressure_in = models.DecimalField(max_digits=5, decimal_places=2),
+    precip_mm = models.DecimalField(max_digits=5, decimal_places=2),
+    precip_in = models.DecimalField(max_digits=5, decimal_places=2),
+    humidity = models.IntegerField(),
+    cloud = models.IntegerField(),
+    feelslike_c = models.DecimalField(max_digits=5, decimal_places=2),
+    feelslike_f = models.DecimalField(max_digits=5, decimal_places=2),
+    vis_km = models.DecimalField(max_digits=5, decimal_places=2),
+    vis_miles = models.DecimalField(max_digits=5, decimal_places=2),
+    uv = models.DecimalField(max_digits=5, decimal_places=2),
+    gust_mph = models.DecimalField(max_digits=5, decimal_places=2),
+    gust_kph = models.DecimalField(max_digits=5, decimal_places=2)
--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -4,19 +4,18 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Snippets API",
-      default_version='v1',
-      description="Test description",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include('test_app.urls')),
     path('admin/', admin.site.urls),
 ]
